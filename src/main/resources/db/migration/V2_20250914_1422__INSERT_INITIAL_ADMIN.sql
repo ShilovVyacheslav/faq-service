52a517ebda0d22c1578aa9047e8a9bdf6a6694db
ShilovVyacheslav <shilovvaceslav16@gmail.com>
2025-09-14 02:53:05 -0700

    feat: add initial migrations

---
+++ src/main/resources/db/migration/V2_20250914_1422__INSERT_INITIAL_ADMIN.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (
+    fullname,
+    username,
+    email,
+    password,
+    role,
+    active,
+    created_at,
+    updated_at
+) VALUES (
+     'Admin User',
+     'admin',
+     'admin@example.com',
+     '$2a$12$pakMgzzJ6ARl2OWZhQW5pOOJpaDNk3CX1elHKDqg6lMbfQlRDjpPa', --@Password0
+     'ADMIN',
+     TRUE,
+     NOW(),
+     NOW()
+);
