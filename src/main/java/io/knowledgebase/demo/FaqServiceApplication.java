9019952233c765cb701b3aad1be171dd6b7095d3
ShilovVyacheslav <shilovvaceslav16@gmail.com>
2025-09-14 02:12:44 -0700

    Initial commit

---
+++ src/main/java/io/knowledgebase/demo/FaqServiceApplication.java
@@ -0,0 +1,13 @@
+package io.knowledgebase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaqServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/knowledgebase/demo/FaqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.knowledgebase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaqServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
