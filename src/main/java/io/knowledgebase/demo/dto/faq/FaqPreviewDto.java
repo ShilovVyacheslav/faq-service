959737626b5ec72fd5e74b7b1e9551ce042ff35a
ShilovVyacheslav <shilovvaceslav16@gmail.com>
2025-09-16 08:24:46 -0700

    feat: add keywords in preview

--- src/main/java/io/knowledgebase/demo/dto/faq/FaqPreviewDto.java
+++ src/main/java/io/knowledgebase/demo/dto/faq/FaqPreviewDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -12,5 +14,6 @@ import lombok.NoArgsConstructor;
 public class FaqPreviewDto {
     private Long id;
     private String question;
+    private List<String> keywords;
     private Boolean active;
 }
